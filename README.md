bf8b3dfa094c2a8f499962721128a90638b929bb
backfire96 <igor.kuzmanovic.996@gmail.com>
2017-07-17 21:21:04 +0200

    empty project

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # socbook1
 Repository for tech9 summer class - team 1
+
+
 README TEST
---
+++ src/main/java/rs/levi9/socbook1/Application.java
@@ -0,0 +1,8 @@
+package rs.levi9.socbook1;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("TEST");
+	}
+}
