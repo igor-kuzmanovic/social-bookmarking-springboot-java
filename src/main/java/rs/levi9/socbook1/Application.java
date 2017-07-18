7a413a8a42877b143846ec7ee76602d6da194414
backfire96 <igor.kuzmanovic.996@gmail.com>
2017-07-18 10:52:26 +0200

    basic application config

--- src/main/java/rs/levi9/socbook1/Application.java
+++ src/main/java/rs/levi9/socbook1/Application.java
@@ -1,8 +1,15 @@
 package rs.levi9.socbook1;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@SpringBootApplication
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("TEST");
+		SpringApplication.run(Application.class, args);
 	}
+	
 }
