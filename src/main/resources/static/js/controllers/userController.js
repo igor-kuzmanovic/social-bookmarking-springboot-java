98448d364c015270b49eed59e68f37460033c7b3
backfire96 <igor.kuzmanovic.996@gmail.com>
2017-07-18 21:58:32 +0200

    fixed the user controller

--- src/main/resources/static/js/controllers/userController.js
+++ src/main/resources/static/js/controllers/userController.js
@@ -7,9 +7,18 @@
     function UserController(UserService) {
         
         var vm = this;
-        vm.saveUser = saveUser; 
-
+        vm.saveUser = saveUser;
+        vm.userStatus = {};
+        vm.userStatus.id = 1;
+        vm.userStatus.type = "STATUS_ACTIVE";
+        vm.role = {};
+        vm.role.id = 2;
+        vm.role.type = "ROLE_USER";
+        vm.roles = [vm.role];
+        
         function saveUser(user){
+        	user.userStatus = vm.userStatus;
+        	user.roles = vm.roles;
             UserService.saveUser(user);
         }
     };
