9b5cdc53e5baf72cf62dfbde9e6a983b836b4671
Sale95 <dobrilovic47@gmail.com>
2017-07-18 14:43:26 +0200

    added user service

---
+++ src/main/resources/static/js/services/userService.js
@@ -0,0 +1,33 @@
+/**
+ * Created by kurs on 7/18/2017.
+ */
+(function () {
+    angular.module("app")
+        .factory('UserService', UserService);
+
+    UserService.$inject = ['$http', '$q'];
+
+    function UserService($http, $q) {
+
+        var service = {
+            saveUser: saveUser,
+        }
+
+        return service;
+
+        function saveUser(user) {
+            var def = $q.defer();
+            var req = {
+                method: 'POST',
+                url: "users",
+                data: user}
+            $http(req).success(function (data) {
+                def.resolve(data);
+            })
+                .error(function () {
+                    def.reject("Failed");
+                });
+            return def.promise;
+        }
+    }
+} ());
