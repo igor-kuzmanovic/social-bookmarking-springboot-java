4d1b4481b672163e39838594df97a06120fdd226
Igor Q <igor.kuzmanovic.996@gmail.com>
2017-07-19 20:39:49 +0200

    Delete app.js

--- out/production/resources/static/js/app.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    var app = angular.module('app', ['ngRoute', 'ngResource', 'ui.bootstrap']);
-
-    app.config(['$httpProvider', function ($httpProvider) {
-        $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-    }]);
-})();
