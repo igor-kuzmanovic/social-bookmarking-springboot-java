d8f5c7bfd99b61c101fa230768fceb951465af92
Sale95 <dobrilovic47@gmail.com>
2017-07-21 11:15:17 +0200

    tag save and delete logic

--- src/main/java/rs/levi9/socbook1/web/controller/BookmarkController.java
+++ src/main/java/rs/levi9/socbook1/web/controller/BookmarkController.java
@@ -5,19 +5,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import rs.levi9.socbook1.domain.Bookmark;
+import rs.levi9.socbook1.domain.Tag;
 import rs.levi9.socbook1.service.BookmarkService;
+import rs.levi9.socbook1.service.TagService;
 
+import java.awt.print.Book;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/bookmarks")
 public class BookmarkController {
 
     private BookmarkService bookmarkService;
+    private TagService tagService;
 
     @Autowired
-    public BookmarkController(BookmarkService bookmarkService) {
+    public BookmarkController(BookmarkService bookmarkService, TagService tagService) {
         this.bookmarkService = bookmarkService;
+        this.tagService = tagService;
     }
 
     @RequestMapping(method = RequestMethod.GET)
@@ -30,25 +37,56 @@ public class BookmarkController {
         Bookmark bookmark = bookmarkService.findOne(id);
 
         if(bookmark == null) {
-            return new ResponseEntity<Bookmark>(HttpStatus.FOUND);
+            return new ResponseEntity<>(HttpStatus.FOUND);
         }
 
-        return new ResponseEntity<Bookmark>(bookmark, HttpStatus.OK);
+        return new ResponseEntity<>(bookmark, HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Bookmark> save(@RequestBody Bookmark bookmark) {
+        Set<Tag> tagExists = new HashSet<>();
+        for (Tag tag : bookmark.getTags()) {
+            Tag foundTag = tagService.findByName(tag.getName());
+            if (foundTag == null) {
+                tagService.save(tag);
+                tagExists.add(tag);
+            } else {
+                tagExists.add(foundTag);
+            }
+        }
+
+        bookmark.setTags(tagExists);
+
         Bookmark bookmarkForSave = bookmarkService.save(bookmark);
 
         if(bookmarkForSave == null) {
-            return new ResponseEntity<Bookmark>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
-        return new ResponseEntity<Bookmark>(bookmarkForSave, HttpStatus.OK);
+        return new ResponseEntity<>(bookmarkForSave, HttpStatus.OK);
     }
 
     @RequestMapping(path = ("/{id}"), method = RequestMethod.DELETE)
     public ResponseEntity delete(@PathVariable Long id) {
+        Bookmark forDelete = bookmarkService.findOne(id);
+        List<Bookmark> allBookmarks = bookmarkService.findAll();
+        boolean hasBookmark = false;
+
+        for (Tag tag : forDelete.getTags()) {
+            for(Bookmark bookmark: allBookmarks) {
+                Set<Tag> bookmarkTags = bookmark.getTags();
+                for(Tag bookmarkTag: bookmarkTags) {
+                    if(tag.getId() == bookmarkTag.getId()) {
+                        hasBookmark = true;
+                    }
+                }
+            }
+            if(hasBookmark) {
+                tagService.delete(tag.getId());
+            }
+        }
+
         bookmarkService.delete(id);
 
         return new ResponseEntity(HttpStatus.OK);
