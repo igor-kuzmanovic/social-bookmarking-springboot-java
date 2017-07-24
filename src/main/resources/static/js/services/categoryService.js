4876e363b99df6670727d96969eac1214a289404
Sale95 <dobrilovic47@gmail.com>
2017-07-24 11:49:58 +0200

    get, save and delete bookmark

---
+++ src/main/resources/static/js/services/categoryService.js
@@ -0,0 +1,31 @@
+(function () {
+    angular.module("app")
+        .factory('CategoryService', CategoryService);
+
+    CategoryService.$inject = ['$http', '$q'];
+
+    function CategoryService($http, $q) {
+
+        var service = {
+            getCategories: getCategories
+        }
+
+        return service;
+
+        function getCategories() {
+            var def = $q.defer();
+            var req = {
+                method: 'GET',
+                url: "categories"
+            }
+            $http(req).success(function (data) {
+                def.resolve(data);
+            })
+                .error(function () {
+                    def.reject("Failed to GET all");
+                });
+            return def.promise;
+        }
+
+    }
+} ());
