af68665137482fe077a79599d3947b9f4344301b
backfire96 <igor.kuzmanovic.996@gmail.com>
2017-07-26 11:07:04 +0200

    added confirmation messages

--- src/main/resources/static/js/routes/routeConfig.js
+++ src/main/resources/static/js/routes/routeConfig.js
@@ -6,11 +6,11 @@
 
     function config($routeProvider, $locationProvider) {
         $routeProvider
-            // .when('/', {
-            //     templateUrl: '/views/home.html',
-            //     controller: 'BookmarkController',
-            //     controllerAs: 'vm'
-            // })
+            .when('/', {
+                 templateUrl: '/views/home.html',
+                 controller: 'BookmarkController',
+                 controllerAs: 'vm'
+            })
             .when('/home', {
                 templateUrl: '/views/home.html',
                 controller: 'BookmarkController',
