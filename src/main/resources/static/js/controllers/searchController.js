9890a685ca43a0cf87929fd3954ad1d59ea1eab2
backfire96 <igor.kuzmanovic.996@gmail.com>
2017-07-27 10:27:04 +0200

    search page basics

--- src/main/resources/static/js/controllers/searchController.js
+++ src/main/resources/static/js/controllers/searchController.js
@@ -1,46 +1,58 @@
 (function(){
-    angular.module('app')
-        .controller('SearchController', SearchController);
+  angular.module('app')
+    .controller('SearchController', SearchController);
 
-    SearchController.$inject = ['$scope', 'BookmarkService'];
+  SearchController.$inject = ['$scope', 'BookmarkService', 'CategoryService'];
 
-    function SearchController($scope, BookmarkService) {
+  function SearchController($scope, BookmarkService, CategoryService) {
 
-        var vm = this;
-        vm.importBookmark = importBookmark;
-        vm.getPublicBookmarks = getPublicBookmarks;
-        
-        init();
-        
-        function init() {
-            if($scope.$parent.vm.user){
-                getPublicBookmarks($scope.$parent.vm.user.name);
-            };
-        }
-        
-        function getPublicBookmarks(username){
-            getCategories();
-            BookmarkService.getPublicBookmarks(username).then(handleSuccessBookmarks);
-        }
-        
-        function handleSuccessBookmarks(data, status) {
-            vm.bookmarks = data;
-        }
-        
-        function getCategories() {
-            CategoryService.getCategories().then(handleSuccessCategories);
-        }
+    var vm = this;
+    vm.importBookmark = importBookmark;
+    vm.getPublicBookmarks = getPublicBookmarks;
+    vm.selectBookmark = selectBookmark;
+    vm.bookmarks;
+    vm.selectedBookmark;
 
-        function handleSuccessCategories(data, status){
-            vm.categories = data;
-        }
-        
-        function importBookmark(bookmarkId) {
-        	BookmarkService.importBookmark($scope.$parent.vm.user.name, bookmarkId).then(function(response){
-            }, function(error){
-                vm.error = error;
-            }) 
-        }
-      
-    };
+    init();
+
+    function init() {
+      if($scope.$parent.vm.user){
+        getPublicBookmarks($scope.$parent.vm.user.name);
+      };
+    }
+
+    function getPublicBookmarks(username){
+      getCategories();
+      BookmarkService.getPublicBookmarks(username).then(handleSuccessBookmarks);
+    }
+
+    function handleSuccessBookmarks(data, status) {
+      vm.bookmarks = data;
+    }
+
+    function getCategories() {
+      CategoryService.getCategories().then(handleSuccessCategories);
+    }
+
+    function handleSuccessCategories(data, status){
+      vm.categories = data;
+    }
+
+    function importBookmark(bookmarkId) {
+      BookmarkService.importBookmark($scope.$parent.vm.user.name, bookmarkId).then(function(response){
+      }, function(error){
+        vm.error = error;
+      }) 
+    }
+
+    function selectBookmark(bookmark) {
+      if(vm.selectedBookmark == bookmark) {
+        vm.selectedBookmark = null;
+      }
+      else {
+        vm.selectedBookmark = bookmark;
+      }
+    }
+
+  };
 })();
