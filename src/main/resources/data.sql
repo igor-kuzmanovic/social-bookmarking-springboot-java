a0958d996900dfd068355e61a5ba5fc464114f3c
backfire96 <igor.kuzmanovic.996@gmail.com>
2017-07-31 09:34:26 +0200

    data query fixed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,7 +6,7 @@ INSERT INTO `socbook1`.`user_status`(`type`) VALUES ('STATUS_INACTIVE');
 
 INSERT INTO `socbook1`.`tag`(`name`) VALUES ('fast');
 INSERT INTO `socbook1`.`tag`(`name`) VALUES ('modern');
-INSERT INTO `socbook1`.`tag`(`name`) VALUES ('dope');
+INSERT INTO `socbook1`.`tag`(`name`) VALUES ('cool');
 
 INSERT INTO `socbook1`.`category`(`name`) VALUES ('Search engine');
 INSERT INTO `socbook1`.`category`(`name`) VALUES ('Social network');
@@ -21,11 +21,11 @@ INSERT INTO `socbook1`.`user_roles`(`user_id`,`role_id`) VALUES (1,2);
 INSERT INTO `socbook1`.`user_roles`(`user_id`,`role_id`) VALUES (2,2);
 INSERT INTO `socbook1`.`user_roles`(`user_id`,`role_id`) VALUES (3,2);
 
-INSERT INTO `socbook1`.`bookmark`(`title`, `url`, `date`, `description`, `is_public`, `imported`, `category_id`, `user_id`) VALUES ('Google', 'http://www.google.com', '9999-12-31 23:59:59', 'Google home page', TRUE, FALSE, 1, 1);
-INSERT INTO `socbook1`.`bookmark`(`title`, `url`, `date`, `description`, `is_public`, `imported`, `category_id`, `user_id`) VALUES ('Facebook', 'http://www.facebook.com', '9999-12-31 23:59:59', 'Facebook home page', TRUE, FALSE, 2, 2);
-INSERT INTO `socbook1`.`bookmark`(`title`, `url`, `date`, `description`, `is_public`, `imported`, `category_id`, `user_id`) VALUES ('Instagram', 'http://www.instagram.com', '9999-12-31 23:59:59', 'Instagram home page', TRUE, FALSE, 2, 3);
-INSERT INTO `socbook1`.`bookmark`(`title`, `url`, `date`, `description`, `is_public`, `imported`, `category_id`, `user_id`) VALUES ('YouTube', 'http://www.youtube.com', '9999-12-31 23:59:59', 'YouTube home page', TRUE, FALSE, 3, 1);
-INSERT INTO `socbook1`.`bookmark`(`title`, `url`, `date`, `description`, `is_public`, `imported`, `category_id`, `user_id`) VALUES ('Twitter', 'http://www.twitter.com', '9999-12-31 23:59:59', 'Twitter home page', TRUE, FALSE, 2, 3);
+INSERT INTO `socbook1`.`bookmark`(`title`, `url`, `date`, `description`, `is_public`, `imported`, `category_id`, `user_id`) VALUES ('Google', 'http://www.google.com', '2012-07-15 23:59:59', 'Google home page', TRUE, FALSE, 1, 1);
+INSERT INTO `socbook1`.`bookmark`(`title`, `url`, `date`, `description`, `is_public`, `imported`, `category_id`, `user_id`) VALUES ('Facebook', 'http://www.facebook.com', '2013-01-12 23:59:59', 'Facebook home page', TRUE, FALSE, 2, 2);
+INSERT INTO `socbook1`.`bookmark`(`title`, `url`, `date`, `description`, `is_public`, `imported`, `category_id`, `user_id`) VALUES ('Instagram', 'http://www.instagram.com', '2015-11-02 23:59:59', 'Instagram home page', TRUE, FALSE, 2, 3);
+INSERT INTO `socbook1`.`bookmark`(`title`, `url`, `date`, `description`, `is_public`, `imported`, `category_id`, `user_id`) VALUES ('YouTube', 'http://www.youtube.com', '2016-12-03 23:59:59', 'YouTube home page', TRUE, FALSE, 3, 1);
+INSERT INTO `socbook1`.`bookmark`(`title`, `url`, `date`, `description`, `is_public`, `imported`, `category_id`, `user_id`) VALUES ('Twitter', 'http://www.twitter.com', '2011-05-29 23:59:59', 'Twitter home page', TRUE, FALSE, 2, 3);
 
 INSERT INTO `socbook1`.`comment`(`content`, `bookmark_id`) VALUES ('Super kewl search engine', 1);
 INSERT INTO `socbook1`.`comment`(`content`, `bookmark_id`) VALUES ('Super kewl social network', 2);
